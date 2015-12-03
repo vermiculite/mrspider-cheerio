a679a10b316a86859fc6d6dfef3eaf59131d8230
sean <seanowenhayes@gmail.com>
2015-12-03 22:15:56 +0100

    first crack, looks ok to me. Time will tell.

---
+++ index.js
@@ -0,0 +1,5 @@
+var cheerio = require('cheerio');
+module.exports = function(page, spider, next) {
+    page.$ = cheerio.load(page.content);
+    next();
+};
---
+++ test/index.js
@@ -0,0 +1,37 @@
+var mrspiderCheerio = require('..');
+var chai = require('chai');
+var should = chai.should();
+var sinon = require('sinon');
+describe('mrspider-cheerio', function() {
+
+    it('should call next', function() {
+        var html = '<h1>Hi there</h1>';
+        var page = {
+            content: html
+        };
+        var spy = sinon.spy();
+        mrspiderCheerio(page, {}, spy);
+        spy.calledOnce.should.equal(true);
+    });
+
+    it('should add $ property to page', function() {
+        var html = '<h1>Hi there</h1>';
+        var page = {
+            content: html
+        };
+        var spy = sinon.spy();
+        mrspiderCheerio(page, {}, spy);
+        should.exist(page.$);
+    });
+
+
+    it('should add $ property giving a jquery like interface to the dom.', function() {
+        var html = '<h1>Hi there</h1>';
+        var page = {
+            content: html
+        };
+        var spy = sinon.spy();
+        mrspiderCheerio(page, {}, spy);
+        page.$('h1').text().should.equal('Hi there');
+    });
+});
