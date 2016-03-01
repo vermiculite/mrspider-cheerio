f4003373c0325e9082d79baf698db0af482a410b
VERMICULITE <seanowenhayes@gmail.com>
2016-03-01 08:34:42 +0000

    Added push.

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ let through2 = require('through2');
 var cheerio = require('cheerio');
 module.exports = through2.obj(function(page, enc, next) {
     page.$ = cheerio.load(page.content || '');
+    this.push(page);
     next();
 });
